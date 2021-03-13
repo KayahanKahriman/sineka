ed179b553eced4e00c002c9ab40af49a7d3545d7
Kayahan Kahrıman <kayahan@kayahankahriman.com>
2021-03-13 23:08:57 +0300

    mobil versiyon düzenlendi

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,35 @@ var swiper = new Swiper(".swiper-container", {
   },
 });
 /** Swiper End */
+
+/** Mobile Nav Start */
+const toggle_nav_button = document.querySelector("#toggle-nav");
+const mobile_nav = document.querySelector("#mobile-nav");
+
+toggle_nav_button.addEventListener("click", () => {
+  mobile_nav.classList.add("show-mobile-menu");
+});
+
+window.addEventListener("touchend", function (e) {
+  if (mobile_nav != e.target && toggle_nav_button != e.target) {
+    mobile_nav.classList.remove("show-mobile-menu");
+  }
+});
+/** Mobile Nav End */
+
+/** Search Form Start */
+const search_form_wrapper = document.querySelector("#search-form-wrapper");
+const close_search_form = document.querySelector(".close-search-form");
+const toggle_search = document.querySelectorAll(".toggle-search");
+const toggle_search_arr = Array.from(toggle_search);
+
+for (var i = 0; toggle_search_arr.length > i; i++) {
+  toggle_search_arr[i].addEventListener("click", () => {
+    search_form_wrapper.classList.add("show-search-form");
+  });
+}
+
+close_search_form.addEventListener("click", () => {
+  search_form_wrapper.classList.remove("show-search-form");
+});
+/** Search Form End */
